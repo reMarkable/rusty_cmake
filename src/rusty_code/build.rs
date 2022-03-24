31b006735bf32f657475396dad8940a736de4f42
Matvei Klimov <klimatt.gu@gmail.com>
2022-03-24 13:16:01 +0100

    Exmaple with Rust + C++ integration. Builds and runs on rM device with Codex Distro: https://github.com/reMarkable/codex-distro/tree/DOPS-45

--- src/rusty_code/build.rs
+++ src/rusty_code/build.rs
@@ -1,4 +1,6 @@
+
 fn main() {
-    let _build = cxx_build::bridge("src/lib.rs");
+    let _build = cxx_build::bridge("src/lib.rs").compile("cxx_demo");
     println!("cargo:rerun-if-changed=src/lib.rs");
+
 }
